e1cf4589f51f8ae5b197909f90195eaa0d2c729b
carson-king-wtc <ck6100@students.inghamisd.org>
2024-02-01 14:15:06 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+import java.awt.*;
+import java.awt.event.WindowEvent;
 import java.awt.image.PixelGrabber;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.IntBuffer;
 import javax.imageio.ImageIO;
+import javax.swing.*;
+
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.bytedeco.opencv.opencv_core.IplImage;
-import java.awt.Color;
+
 import java.util.LinkedList;
 
 public class Main{
@@ -54,5 +56,48 @@ public class Main{
             }
             writer.close();
         }
+        System.out.println("Ascii done, opening window");
+        JFrame window = new JFrame();
+
+        BufferedImage image = ImageIO.read(new File("frame-dump/video-frame-" + 0 + ".png"));
+        window.setSize(image.getWidth(), image.getHeight());//400 width and 500 height
+
+        window.setLayout(null);//using no layout managers
+        window.setVisible(true);//making the frame visible
+        JLabel text = new JLabel("", SwingConstants.CENTER);
+        text.setLocation(new Point(image.getWidth()/2, image.getHeight()/2));
+        window.add(text);
+        for(int i=0;i<frameNumber;i++) {
+            String everything = "";
+            try {
+                BufferedReader br = new BufferedReader(new FileReader("frame-ascii/frame"+i+".txt"));
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                everything = sb.toString();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+            //change the window size and other stuff
+
+            text.setText("<html>" + everything.replaceAll("<","&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br/>") + "</html>");
+            //System.out.println(text.getX());
+
+            try {
+                Thread.sleep(41);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
     }
 }
