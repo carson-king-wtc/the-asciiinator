21eade8de69606a82f8680f641fc3f34fe3a73f4
carson-king-wtc <ck6100@students.inghamisd.org>
2024-02-01 12:41:18 -0500

    fr fr

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,45 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.IntBuffer;
 import javax.imageio.ImageIO;
-import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber.Exception;
-public class Main {
-    public static void main(String[] args) {
-        FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber("C:/Users/Digilog/Downloads/Test.mp4");
-        frameGrabber.start();
-        IplImage i;
-        try {
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.opencv.opencv_core.IplImage;
 
-            i = frameGrabber.grab();
-            BufferedImage  bi = i.getBufferedImage();
-            ImageIO.write(bi,"png", new File("D:/Img.png"));
-            frameGrabber.stop();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+public class Main{
+    public static void main(String []args) throws IOException, Exception
+    {
+        //set up a frame grabber
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("bad apple.mp4");
+        grabber.start();
+
+        //setup the converter, this lets us turn frames into image data
+        Java2DFrameConverter converter = new Java2DFrameConverter();
+
+        try {
+            //too lazy to find out how to get the number of frames, just go a long time instead.
+            for (int i = 0; i < 1000000; i++) {
+                System.out.println("got frame #"+i);
+                Frame frame = grabber.grabImage(); // It is important to use grabImage() to get a frame that can be turned into a BufferedImage
+
+                BufferedImage bi = converter.convert(frame); //convert the frame into image data
+
+                //write the file
+                ImageIO.write(bi, "png", new File("C:/Users/ck6100/Documents/GitHub/Bad Apple/bad-apple-but-uhhhh/frame-dump/video-frame-" + i + ".png"));
+            }
         }
+        catch (Exception e)
+        {
+
+        }
+        System.out.println("done!");
+        //stop the grabber
+        grabber.stop();
+
+
     }
 }
